fd10a32cd311e7da830700a049b167814140fe06
RY-000 <reynardswijaya@gmail.com>
2024-11-29 00:56:15 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('loginForm').addEventListener('submit', function(event){
+    event.preventDefault();
+    alert('Login functionality not implemented yet!');
+});
+
+document.getElementById('registerForm').addEventListener('submit', function(event){
+    event.preventDefault();
+    alert('Registration functionality not implemented yet!');
+});
